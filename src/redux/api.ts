eabacb96d5f0dd96f38a47044225c6fd3d927702
Aleksandr Sharipov <sharipov.sasha.spb@yandex.ru>
2022-08-16 15:12:42 +0300

    new socket connect

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,5 +1,6 @@
+// @ts-nocheck
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
 export type Channel = 'redux' | 'general'
 
@@ -12,13 +13,24 @@ interface PhoneNumber {
 
 console.log(process.env.REACT_APP_API_URL)
 
+let socket: Socket;
+function getSocket() {
+  if (!socket) {
+    socket = io(process.env.REACT_APP_API_URL, {
+      withCredentials: true,
+    });
+  }
+  return socket;
+}
+
 export const myApi = createApi({
   reducerPath: 'myApi',
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
   endpoints: (build) => ({
     sendMessage: build.mutation<PhoneNumber, string>({
       queryFn: (phoneNumber: string) => {
-        const socket = io();
+        // const socket = io();
+        const socket = getSocket();
         return new Promise(resolve => {
           socket.emit('newPhoneNumber', phoneNumber, (fullNumber: PhoneNumber) => {
             resolve({ data: fullNumber });
@@ -35,7 +47,9 @@ export const myApi = createApi({
       ) {
         try {
           await cacheDataLoaded;
-          const socket = io();
+          // const socket = io();
+          const socket = getSocket();
+
 
           socket.on('connection', () => {
             socket.emit('result-number');
